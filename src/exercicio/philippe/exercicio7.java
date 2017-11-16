79097dfa39330c02f727628dd6db99770be74a77
odanilomoreira <danilokof7@gmail.com>
2017-11-16 01:29:05 -0200

    exercicio 7

---
+++ src/exercicio/philippe/exercicio7.java
@@ -0,0 +1,30 @@
+package exercicio.philippe;
+
+import java.util.Scanner;
+
+public class exercicio7 {
+
+	public static void main(String[] args) {
+		
+		int a,b,exp;
+		
+		System.out.println("\nEntre com o valor de a: ");
+		Scanner input = new Scanner(System.in);
+		a = input.nextInt();
+		
+		System.out.println("\nEntre com o valor de b: ");
+		b = input.nextInt();
+		
+		input.close();
+		
+		System.out.println("\nResultado de a^b: ");
+		exp = exponencial(a,b);
+		System.out.println(exp);
+	}
+	
+	
+	public static int exponencial(int a, int b) {
+		return (int) Math.pow(a, b);
+	}
+
+}
